33883245b5a99ba6997060d63ce790a3f0481d36
Ryan de Jonge <r.dejonge@provrex.nl>
2024-06-21 14:09:53 +0200

    Fully functional bingo creation menu

--- resources/js/Pages/Create.jsx
+++ resources/js/Pages/Create.jsx
@@ -38,6 +38,32 @@ export default function Create({ auth }) {
         reader.readAsDataURL(files[0]);
     }
 
+    function addImageItem(files)
+    {
+        let allowedNewItems = 24 - items.length;
+        if(files.length > allowedNewItems)
+        {
+            alert('You can only add ' + allowedNewItems + ' new items.');
+            return;
+        }
+
+        let newItems = [];
+        for(let i = 0; i < files.length; i++)
+        {
+            let reader = new FileReader();
+            reader.onload = (e) =>
+            {
+                newItems = [...newItems, {title: '', description: '', icon: e.target.result}];
+                console.log(newItems);
+                if(newItems.length === files.length)
+                {
+                    setItems([...items, ...newItems]);
+                }
+            };
+            reader.readAsDataURL(files[i]);
+        }
+    }
+
     function removeItem(i)
     {
         setItems(items.filter((item, index) => index !== i));
@@ -75,78 +101,101 @@ export default function Create({ auth }) {
                             <p className="mt-1 max-w-2xl text-sm text-gray-500 dark:text-gray-400">Upload 24 images, and give your bingo card a name and description.</p>
                             {/* disable form for js */}
                             <form onSubmit={(e) => submit(e)} >
+
+                                {form.icon && <img src={form.icon} className="w-24 h-24" />}
+
+                                <div className="mt-5">
+                                    <label htmlFor="icon" className="text-sm font-medium text-white dark:text-gray-200 inline-flex items-center px-4 py-2 border border-transparent rounded-md bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-pointer">
+                                        Set Icon
+                                    </label>
+                                    <input type="file" name="icon" id="icon" className="hidden"
+                                        onChange={(e) => processIcon(e.target.files)}
+                                    />
+                                </div>
                                 <div className="mt-5">
                                     <label htmlFor="name" className="block text-sm font-medium text-gray-700 dark:text-gray-200">Name</label>
                                     <input type="text" name="name" id="name" className="mt-1 block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
-                                        onChange={(e) => setForm({...form, name: e.target.value})}
+                                        onChange={(e) => setForm({ ...form, name: e.target.value })}
                                         value={form.name}
                                         required
                                     />
                                 </div>
                                 <div className="mt-5">
                                     <label htmlFor="description" className="block text-sm font-medium text-gray-700 dark:text-gray-200"
-                                        onChange={(e) => setForm({...form, description: e.target.value})}
+                                        onChange={(e) => setForm({ ...form, description: e.target.value })}
                                         value={form.description}
-                                        >Description</label>
+                                    >Description</label>
                                     <textarea type="text" name="description" id="description" className="mt-1 block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
-                                        onChange={(e) => setForm({...form, description: e.target.value})}
+                                        onChange={(e) => setForm({ ...form, description: e.target.value })}
                                         value={form.description}
                                         required
                                     />
                                 </div>
 
-                                {form.icon && <img src={form.icon} className="w-24 h-24" />}
-
-                                <div className="mt-5">
-                                    <label htmlFor="icon" className="block text-sm font-medium text-gray-700 dark:text-gray-200">Icon</label>
-                                    <input type="file" name="icon" id="icon" className="mt-1 block w-full sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300"
-                                        onChange={(e) => processIcon(e.target.files)}
-                                    />
-                                </div>
-
-                                <div className="mt-5">
-                                    <button type="button" className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                        onClick={() => items.length < 24? setItems([...items, {title: '', description: '', icon: ''}]) : null}
-                                    >
-                                        Add Item
-                                    </button>
-                                </div>
-
-                                {
-                                    items.map((item, i) =>
-                                    {
-                                        return <div className="mt-5 drop-shadow-md bg-slate-100 p-4" key={"item-" + i}>
-                                            <label htmlFor={'title-' + i} className="block text-sm font-medium text-gray-700 dark:text-gray-200">Title</label>
-                                            <input type="text" name={'title-' + i} id={'title-' + i} className="mt-1 block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
-                                                onChange={(e) => updateItem({...item, title: e.target.value}, i)}
-                                                value={item.title}
-                                            />
-                                            <label htmlFor={'description-' + i} className="block text-sm font-medium text-gray-700 dark:text-gray-200">Description</label>
-                                            <input type="text" name={'description-' + i} id={'description-' + i} className="mt-1 block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
-                                                onChange={(e) => updateItem({...item, description: e.target.value}, i)}
-                                                value={item.description}
-                                            />
-                                            <label htmlFor={'img-' + i} className="block text-sm font-medium text-gray-700 dark:text-gray-200 mt-2">(Optional) Icon</label>
-                                            <input type="file" name={'img-' + i} id={'img-' + i} className="mt-1 mb-1 block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
-                                                onChange={(e) => setItemImage(e.target.files, item, i)}
-                                            />
-                                            {item.icon && <img src={item.icon} className="w-24 h-24" />}
-                                            <button type="button" className="mt-2 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
-                                                onClick={() => removeItem(i)}
-                                            >
-                                                Remove
-                                            </button>
-                                        </div>
-                                    })
-                                }
-
                                 <div className="mt-5">
                                     Items: {items.length} / 24
                                 </div>
                                 <div className="mt-5">
-                                    <button disabled={items.length !== 24} type="submit" className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:bg-indigo-300">
-                                        {items.length === 24 ? 'Create' : 'Please create 24 items.'}
+                                    <button disabled={items.length !== 24 || form.icon === ''} type="submit" className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 disabled:bg-green-300 disabled:cursor-not-allowed">
+                                        {items.length === 24 ? 'Create!' : 'Please create 24 items and set an icon.'}
                                     </button>
+                                </div>
+
+                                <div className="mt-5">
+                                    <button type="button" className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 "
+                                        onClick={() => items.length < 24 ? setItems([...items, { title: '', description: '', icon: '' }]) : null}
+                                    >
+                                        Add Item
+                                    </button>
+                                    <label htmlFor="addimg" className="ml-2 cursor-pointer text-sm font-medium text-white dark:text-gray-200 inline-flex items-center px-4 py-2 border border-transparent rounded-md bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                        Add Image Item(s)
+                                    </label>
+                                    <input type="file" name="addimg" id="addimg" className="hidden"
+                                        onChange={(e) => addImageItem(e.target.files)}
+                                        multiple
+                                    />
+                                    {/* clear button */}
+                                    <button type="button" className="ml-2 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                                        onClick={() => setItems([])}
+                                    >
+                                        Clear
+                                    </button>
+                                </div>
+
+                                {
+                                    items.length < 1 && <div className="mt-5">
+                                        <p className="text-sm font-medium text-red-700">No items added yet.</p>
+                                    </div>
+                                }
+
+                                <div className="mt-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                                    {
+                                        items.map((item, i) =>
+                                        {
+                                            return <div className="drop-shadow-md bg-slate-100 p-4" key={"item-" + i}>
+                                                <label htmlFor={'title-' + i} className="block text-sm font-medium text-gray-700 dark:text-gray-200">Title</label>
+                                                <input type="text" name={'title-' + i} id={'title-' + i} className="mt-1 block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                                                    onChange={(e) => updateItem({ ...item, title: e.target.value }, i)}
+                                                    value={item.title}
+                                                />
+                                                <label htmlFor={'description-' + i} className="block text-sm font-medium text-gray-700 dark:text-gray-200">Description</label>
+                                                <input type="text" name={'description-' + i} id={'description-' + i} className="mt-1 block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                                                    onChange={(e) => updateItem({ ...item, description: e.target.value }, i)}
+                                                    value={item.description}
+                                                />
+                                                <label htmlFor={'img-' + i} className="block text-sm font-medium text-gray-700 dark:text-gray-200 mt-2">(Optional) Icon</label>
+                                                <input type="file" name={'img-' + i} id={'img-' + i} className="mt-1 mb-1 block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                                                    onChange={(e) => setItemImage(e.target.files, item, i)}
+                                                />
+                                                {item.icon && <img src={item.icon} className="w-24 h-24" />}
+                                                <button type="button" className="mt-2 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                                                    onClick={() => removeItem(i)}
+                                                >
+                                                    Remove
+                                                </button>
+                                            </div>
+                                        })
+                                    }
                                 </div>
                             </form>
                         </div>
